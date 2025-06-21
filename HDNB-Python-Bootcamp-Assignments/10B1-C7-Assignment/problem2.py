9049743211e756bd263444b58c025e6ebc9265e6
Md Farhan Mubasshir Taha <mubasshirtaha.business@gmail.com>
2025-06-21 19:54:01 +1000

    Minor updates to Problem 2 file for Class 7 Assignment

--- HDNB-Python-Bootcamp-Assignments/10B1-C7-Assignment/problem2.py
+++ HDNB-Python-Bootcamp-Assignments/10B1-C7-Assignment/problem2.py
@@ -3,10 +3,10 @@
 import datetime
 
 # Current date and time
-now : datetime = datetime.datetime.now()
+now = datetime.datetime.now()
 print(f"Current date and time: {now}")
 
 # Future date and time (30 days from now)
-future : datetime = now + datetime.timedelta(days=30)
+future = now + datetime.timedelta(days=30)
 print(f"Date and time 30 days from now: {future}")
 
