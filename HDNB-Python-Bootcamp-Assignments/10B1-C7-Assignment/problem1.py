0fe8f0b007e9f50c1f50b25d957aa66f8918d339
Md Farhan Mubasshir Taha <mubasshirtaha.business@gmail.com>
2025-06-21 19:30:43 +1000

    Class 7 Assignment completed partially

---
+++ HDNB-Python-Bootcamp-Assignments/10B1-C7-Assignment/problem1.py
@@ -0,0 +1,16 @@
+# Problem 1: Time Elapsed Since Task Started
+
+import time
+
+print('Please click "Enter" to start Timer ')
+input()
+
+startTime = time.time()
+
+task = time.sleep(5)
+
+timePassed = time.time() - startTime
+
+# print(f'Time passed: {timePassed} seconds')
+print(f"Time passed: {round(timePassed, 2)} seconds")
+# round() gives a cleaner output hence its been used
